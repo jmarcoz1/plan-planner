4218d5fbc96a10cb9bbece86fbc02332290a4937
Jorge Marco <jmararr1@gmail.com>
2024-01-07 11:59:09 +0100

    can get data from backend

--- src/pages/AddPlans.js
+++ src/pages/AddPlans.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useState } from 'react'
+import { getActivities } from '../API/API'
+import { Box } from '@mui/system'
+import { Button } from '../styles/StyledComponents';
 
 const AddPlans = () => {
-  console.log('add plans')
+  const [data, setData] = useState(null)
+
+  const handleClick = async () => {
+    const information = await getActivities();
+    setData(information)
+    console.log({information})
+  }
+
   return (
-    <div>AddPlans</div>
+    <Box sx={{ display: "flex", flexDirection: "column" }} >
+      <Button variant="contained" onClick={handleClick}>get data</Button>
+    </Box>
   )
 }
 
